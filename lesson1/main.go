7e1e9bfaa64e966bf893cb33f9daaff6100cfb08
Yavgeniy <evgeniyv6@gmail.com>
2021-01-18 14:04:38 +0300

    correct fmt

--- lesson1/main.go
+++ lesson1/main.go
@@ -10,7 +10,7 @@ func main() {
 
 	// try 14 exercise
 	fmt.Println("Automorphic numbers:")
-	for i:=1; i< 2890625; i++ {
+	for i := 1; i < 2890625; i++ {
 		if c_algos.IsAutomorph(i) {
 			fmt.Printf("%d\t", i)
 		}
@@ -21,24 +21,18 @@ func main() {
 	// try 13 exercise
 	fmt.Println("Random numbers:")
 	fmt.Printf("Standart rand func: %d\n", c_algos.StandartRand(1, 100))
-	fmt.Printf("Lehmer rand func: %d\n",c_algos.KnuthRand(1, 100,4,5))
+	fmt.Printf("Lehmer rand func: %d\n", c_algos.KnuthRand(1, 100, 4, 5))
 
 	fmt.Println()
 	// try 12 exercise
-	fmt.Printf("Max: %d\n",c_algos.MaxFromInt(1,2,3))
-	fmt.Printf("Min: %d\n",c_algos.MinFromInt(1,-122,-3))
+	fmt.Printf("Max: %d\n", c_algos.MaxFromInt(1, 2, 3))
+	fmt.Printf("Min: %d\n", c_algos.MinFromInt(1, -122, -3))
 	fmt.Println()
 
 	// try 6 exercise
-	for i:=1; i<=150;i++{
+	for i := 1; i <= 150; i++ {
 		age, yearDes := c_algos.YearIdentifier(i)
 		fmt.Printf("age - %d  %s # ", age, yearDes)
 	}
 
 }
-
-
-
-
-
-
